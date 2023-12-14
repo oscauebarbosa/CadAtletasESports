a7048e4f71b592c12f91e7626598d06840d8892e
oscauebarbosa <124931002+oscauebarbosa@users.noreply.github.com>
2023-12-14 14:20:06 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = 'Senai'
 
 class cadESports:
     def __init__(self, nome, jogo, funcao, ranking):
@@ -13,23 +14,34 @@ lista = []
 
 @app.route('/atletas')
 def atletaseSports():
-    return render_template('ESports.html', Titulo ="Atletas eSports: ", ListaAtletas = lista)
+    if 'Usuario_Logado' not in session:
+        return redirect('/')
+    else:
+        return render_template('ESports.html', Titulo ="Atletas eSports: ", ListaAtletas = lista)
 
 
 @app.route('/cadastro')
 def cadastro():
-    return render_template('Cadastro.html', Titulo = "Cadastro de Atletas")
+    if 'Usuario_Logado' not in session:
+        return redirect('/')
+    else:
+        return render_template('Cadastro.html', Titulo = "Cadastro de Atletas")
 
 
 @app.route('/criar', methods= ['POST'])
 def criar():
-    nome = request.form['nome']
-    jogo = request.form['jogo']
-    funcao = request.form['funcao']
-    ranking = request.form['ranking']
-    obj = cadESports(nome, jogo, funcao, ranking)
-    lista.append(obj)
-    return redirect('/atletas')
+    if 'salvar' in request.form:
+        nome = request.form['nome']
+        jogo = request.form['jogo']
+        funcao = request.form['funcao']
+        ranking = request.form['ranking']
+        obj = cadESports(nome, jogo, funcao, ranking)
+        lista.append(obj)
+        return redirect('/atletas')
+    elif 'deslogar' in request.form:
+        return redirect('/')
+
+
 
 @app.route('/excluir/<nomeatletas>', methods=['GET','DELETE'])
 def excluir(nomeatletas) :
@@ -55,6 +67,24 @@ def alterar():
             atletas.funcao = request.form['funcao']
             atletas.ranking = request.form['ranking']
     return redirect('/atletas')
+
+
+@app.route('/')
+def login():
+    session.clear()
+    return render_template('Login.html', Titulo = "Faça seu login")
+
+
+@app.route('/autenticar', methods = ['POST'])
+def autenticar():
+    if request.form['usuario'] == 'Caue' and request.form['senha']=='123':
+        session['Usuario_Logado'] = request.form['usuario']
+        flash('Usuario Logado com Sucesso')
+        return redirect('/cadastro')
+    else:
+        flash('Usuario não encontrado')
+        return redirect('/login')
+
 
 
 
